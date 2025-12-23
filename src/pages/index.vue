ec8660f0d331afd22d9a155a9a666dbff20188a2
Sebastian Berlin <berlin.sebastian@gmail.com>
2025-12-23 16:58:30 +0100

    Prepare for Github Pages deployment

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <IngredientsTable />
+    <div>Uses data from <a href="https://www.livsmedelsverket.se/om-oss/psidata/livsmedelsdatabasen">Livsmedelsverket's API</a> licensed under <a href="https://creativecommons.org/licenses/by/4.0/legalcode.sv">CC-BY</a></div>
 </template>
 
 <script lang="ts" setup>
--- vite.config.mts
+++ vite.config.mts
@@ -67,4 +67,5 @@ export default defineConfig({
     server: {
         port: 3000,
     },
+    base: "/food-bits/"
 });
